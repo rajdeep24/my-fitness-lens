7809cc3da8f4b6c47c80d4a4eee772d240d0c08a
Developer Raj <rajdeepkakar@gmail.com>
2020-10-21 19:53:07 -0400

    Updated server.js file with the middleware path to static public folder.

--- server.js
+++ server.js
@@ -10,6 +10,8 @@ const PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+app.use(express.static("public"));
+
 //MONGOOSE CONNECTION
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
 	useNewUrlParser: true,
